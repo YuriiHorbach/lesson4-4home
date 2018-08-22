4e1bc8484dc1ef9c81bcac1a28c32b93dd563e82
southl <gyrii84@gmail.com>
2018-08-22 17:29:30 +0300

    try to fix arrows in slick

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
-  $('.slider').slick();
+  $('.slider').slick({
+  	 appendArrows: $('.feedback__left'),
+            prevArrow: '<button id="prev" type="button" class="btn btn-juliet"><i class="fa fa-chevron-left" aria-hidden="true"></i> Туда</button>',
+     appendArrows: $('.feedback__right'),
+            nextArrow: '<button id="next" type="button" class="btn btn-juliet">Сюда <i class="fa fa-chevron-right" aria-hidden="true"></i></button>'
+  });
+ 
 });
