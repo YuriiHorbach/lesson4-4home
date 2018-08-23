e7afd266918bd488e8df85cd15b80e0f726d07ca
southl <gyrii84@gmail.com>
2018-08-23 11:31:50 +0300

    add prev next to slider https://stackoverflow.com/questions/29876185/change-the-arrow-buttons-in-slick-slider

--- js/main.js
+++ js/main.js
@@ -1,9 +1,7 @@
 $(document).ready(function(){
   $('.slider').slick({
-  	 appendArrows: $('.feedback__left'),
-            prevArrow: '<button id="prev" type="button" class="btn btn-juliet"><i class="fa fa-chevron-left" aria-hidden="true"></i> Туда</button>',
-     appendArrows: $('.feedback__right'),
-            nextArrow: '<button id="next" type="button" class="btn btn-juliet">Сюда <i class="fa fa-chevron-right" aria-hidden="true"></i></button>'
+     prevArrow: $('.prev'),
+     nextArrow: $('.next'),     
   });
  
 });
