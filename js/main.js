0935b6253dea415e5d41ce0af7141cf29568cdbe
southl <gyrii84@gmail.com>
2018-08-25 12:35:33 +0300

    try to add popup after send message

--- js/main.js
+++ js/main.js
@@ -13,5 +13,35 @@ $(document).ready(function(){
     //открыть модальное окно с id="myModal"
     $('#myModal').modal('show')
   });
+
+
+  var $page = $('html, body');
+  $('a[href*="#"]').click(function(){
+  	$page.animate({
+  		scrollTop:$($.attr(this,'href')).offset().top
+  	},400);
+  	return false;
+  });
+
+
+
+
+  $("button#submit").click(function(){
+$.ajax({
+type: "POST",
+url: "feedback.php",
+data: $('form.feedback').serialize(),
+success: function(message){
+$("#feedback").html(message)
+$("#feedback-modal").modal('hide');
+},
+error: function(){
+alert("Error");
+}
+});
+});
+
+	
+
  
 });
